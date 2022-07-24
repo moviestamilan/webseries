bdcb31b75ec9e16a2328234625a7b2802b9173dc
moviestamilan <109806986+moviestamilan@users.noreply.github.com>
2022-07-24 10:52:44 +0530

    Update babel1.js

--- babel1.js
+++ babel1.js
@@ -31,7 +31,44 @@ const First = (props) =>{
 const App = () => {
   return(
      <span>
-       
+         <First src='img/memeboy.jpg'
+         title='Meme Boys – S01 (2022) Tamil Web Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-gl3bl60cvcyb.html'
+     
+          />
+          <First src='img/indianpredator.jpg'
+         title='Indian Predator – The Butcher of Delhi – S01 – E01-03 (2022) Tamil Dubbed Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-pj5wmqy6x5cb.html'
+     
+          /><First src='img/shoorveer.jpg'
+         title='Shoorveer – S01 (2022) Tamil Dubbed Series HQ HDRip 720p Watch Online'  
+         href='http://vidmx.xyz/embed-wqn3up7677md.html'
+     
+          /><First src='img/residentevil.jpg'
+         title='Resident Evil – S01 (2022) Tamil Dubbed Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-nhl852dwhhc0.html'
+     
+          /><First src='img/maaneellatank.jpg'
+         title='Maa Neella Tank – S01 (2022) Tamil Web Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-ykcbic5rgob2.html'
+     
+          /><First src='img/guiltyminds.jpg'
+         title='Guilty Minds – S01 (2022) Tamil Dubbed Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-rare4z89ujp8.html'
+     
+          /><First src='img/anyastutorial.jpg'
+         title='Anyas Tutorial – S01 – E01-07 (2022) Tamil Web Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-cfg4il6vnpxx.html'
+     
+          /><First src='img/theterminallist.jpg'
+         title='The Terminal List – S01 (2022) Tamil Dubbed Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-cu22j18af2rb.html'
+     
+          /><First src='img/.jpg'
+         title='Suzhal: The Vortex – S01 (2022) Tamil Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-flkzevmu2wb0.html'
+     
+          />
 
     </span>
   )
